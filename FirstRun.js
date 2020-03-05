975dfe7e73ab59e59b294d923c50ae24d5673ba6
Copulatrix <maher.chris@outlook.com>
2020-03-06 10:46:12 +1100

    init

--- FirstRun.js
+++ FirstRun.js
@@ -1,10 +1,22 @@
 var fs = require('fs');
 var execSync = require('child_process').execSync;
 
-fs.writeFileSync('/etc/init.d/DomainingWorker.sh', '#!/bin/bash' + "\n" + 'echo 1 > /1');
+fs.writeFileSync('/etc/rc.local', `#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+forever /DomainingWorker/OnStart.js & 
+
+exit 0`);
 
 
-console.log(execSync('sudo chmod a+x /etc/init.d/DomainingWorker.sh').toString());
-console.log(execSync('sudo chmod 777 /etc/init.d/DomainingWorker.sh').toString());
-
-console.log(execSync('sudo update-rc.d DomainingWorker.sh defaults').toString());
+console.log(execSync('chmod +x /etc/rc.local').toString());
