261b3d7b5999738b5c6bbf1bb2f3376db65fecc8
Copulatrix <maher.chris@outlook.com>
2020-03-06 10:35:09 +1100

    init

--- FirstRun.js
+++ FirstRun.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var execSync = require('child_process').execSync;
 
-fs.writeFileSync('/etc/init.d/DomainingWorker.sh', '#!/bin/bash' + "\n" + 'forever start /DomainingWorker/OnStart.js');
+fs.writeFileSync('/etc/init.d/DomainingWorker.sh', '#!/bin/bash' + "\n" + 'echo 1 > /1');
 
 console.log(execSync('sudo update-rc.d DomainingWorker.sh start 2').toString());
 console.log(execSync('sudo chmod +x /etc/init.d/DomainingWorker.sh').toString());
