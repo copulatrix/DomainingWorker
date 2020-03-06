3bf027718b4b4ab00db78f34f6aedf18c0f2e28f
Copulatrix <maher.chris@outlook.com>
2020-03-06 11:10:16 +1100

    init

--- FirstRun.js
+++ FirstRun.js
@@ -1,5 +1,12 @@
+var _Self = require('./App.json');
+var _Secret = _Self[0];
+var _Name = _Self[1];
+var _Hostname = _Self[2];
+
 var fs = require('fs');
 var execSync = require('child_process').execSync;
+
+var request = require('request');
 
 fs.writeFileSync('/etc/rc.local', `#!/bin/sh -e
 #
@@ -18,5 +25,15 @@ forever /DomainingWorker/OnStart.js &
 
 exit 0`);
 
-
 console.log(execSync('chmod +x /etc/rc.local').toString());
+
+request.post({
+    url: `https://${_Secret}:${_Secret}@domaining.fadebit.com/api/worker/notify`,
+    form: {
+        Name: _Name,
+        Hostname: _Hostname
+    }
+}, function(error, response, body){
+    console.log(error);
+    console.log(body);
+});
