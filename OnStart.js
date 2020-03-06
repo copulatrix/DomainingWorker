13a1534447f136dab91e5712db58419ed06fe64e
Copulatrix <maher.chris@outlook.com>
2020-03-06 11:39:09 +1100

    init

--- OnStart.js
+++ OnStart.js
@@ -5,11 +5,35 @@ var _Hostname = _Self[2];
 
 var express = require('express');
 var request = require('request');
+var exec = require('child_process').exec;
 
 var app = express();
  
 app.get('/', function(req, res){
     res.send('Hello World');
+});
+ 
+app.get('/update', function(req, res){
+    exec('git pull', {
+        cwd: __dirname
+    }, function(GITerror, GITstdout, GITstderr){
+        exec('npm install', {
+            cwd: __dirname
+        }, function(NPMerror, NPMstdout, NPMstderr){
+            res.send({
+                git: {
+                    error: GITerror,
+                    stdout: GITstdout,
+                    stderr: GITstderr
+                },
+                npm: {
+                    error: NPMerror,
+                    stdout: NPMstdout,
+                    stderr: NPMstderr
+                }
+            });
+        });
+    });
 });
  
 app.listen(3000);
