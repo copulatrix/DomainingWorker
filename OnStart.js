ba4c0dd767abb64e3bd7d8e79c8c0da21d6e880f
Copulatrix <maher.chris@outlook.com>
2020-03-06 11:21:31 +1100

    init

--- OnStart.js
+++ OnStart.js
@@ -1,8 +1,27 @@
-const express = require('express')
-const app = express()
+var _Self = require('./App.json');
+var _Secret = _Self[0];
+var _Name = _Self[1];
+var _Hostname = _Self[2];
+
+var express = require('express');
+var request = require('request');
+
+var app = express();
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
  
-app.listen(3000)
+app.listen(3000);
+
+request.post({
+    url: `https://${_Secret}:${_Secret}@domaining.fadebit.com/api/worker/notify`,
+    form: {
+        Name: _Name,
+        Hostname: _Hostname,
+        Status: 'StartUp'
+    }
+}, function(error, response, body){
+    console.log(error);
+    console.log(body);
+});
